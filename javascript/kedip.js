bdeff3c0a12c63e922302fbfd9bf95f3903b64e6
pemri123 <55519163+pemri123@users.noreply.github.com>
2019-10-25 11:07:15 +0700

    Update kedip.js

--- javascript/kedip.js
+++ javascript/kedip.js
@@ -1,4 +1,4 @@
-var kedipan = 200; 
+var kedipan = 400; 
 var dumet = setInterval(function () {
     var ele = document.getElementById('textkedip');
     ele.style.visibility = (ele.style.visibility == 'hidden' ? '' : 'hidden');
