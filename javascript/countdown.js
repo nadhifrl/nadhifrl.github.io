0e65ebc22e7aaa94a3a78f99c3ad704c1a6103ac
pemri123 <55519163+pemri123@users.noreply.github.com>
2019-10-25 10:47:03 +0700

    Create countdown.js

---
+++ javascript/countdown.js
@@ -0,0 +1,14 @@
+var target_date = new Date("oct 27, 2019").getTime();
+    var days, hours, minutes, seconds;
+    var countdown = document.getElementById("countdown");
+    setInterval(function () {
+        var current_date = new Date().getTime();
+        var seconds_left = (target_date - current_date) / 1000;
+        days = parseInt(seconds_left / 86400);
+        seconds_left = seconds_left % 86400;
+        hours = parseInt(seconds_left / 3600);
+        seconds_left = seconds_left % 3600;
+        minutes = parseInt(seconds_left / 60);
+        seconds = parseInt(seconds_left % 60);
+        countdown.innerHTML = days + " <span class='digit'>hari</span> " + hours + " <span class='digit'>jam</span> " + minutes + " <span class='digit'>menit</span> " + seconds
+    }, 1000);
