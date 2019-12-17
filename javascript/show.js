0297bc64aeaac727d1c9b948237b507fd2a80ecd
nadhifrl <55243634+nadhifrl@users.noreply.github.com>
2019-12-17 15:49:40 +0700

    Update show.js

--- javascript/show.js
+++ javascript/show.js
@@ -17,25 +17,44 @@ var masukanpass = document.getElementById('Password'),
  
      }
 
-function Login() {
-    var nama = document.getElementById("Username").value;
-    var password = document.getElementById("Password").value;
-    if (nama != "pemri123" && password !="facebook123") {
-         alert('Username dan Password anda salah');
-    }else{
-         alert('Login Berhasil');
-         document.location="home.html"
+     function Login() {
+        var nama = document.getElementById("Username").value;
+        var password = document.getElementById("Password").value;
+        if (nama != "pemri123" && password !="facebook123") {
+            alert('Username dan Password anda salah');
+        }else{
+            alert('Login Berhasil');
+            document.location="home.html"   
             
+        }
     }
-}
 
-function validate() {
-    var password1 = document.getElementById('pw1').value;
-    var password2 = document.getElementById('pw2').value;
-    if (password1 != password2) {
-        alert("Password Tidak Sama!!");
-    } else {
-        alert("Selamat Anda Telah Mengganti Password Anda");
-        document.location="index.html"
+    function validate() {
+        var password1 = document.getElementById('pw1').value;
+        var password2 = document.getElementById('pw2').value;
+        if (password1 != password2) {
+            alert("Password Tidak Sama!!");
+        } else {
+            alert("Selamat Anda Telah Mengganti Password Anda");
+            document.location="login.html"
+        }
     }
-}
+
+    function check_empty() {
+        var email=document.getElementById('email').value;
+        if (email == "") {
+        alert("No email address specified.");
+        } else {
+        document.getElementById('forgetpass').submit("login.html");
+        alert("Your password has been reset, please check Your email");
+        window.location.href = "login.html";
+        }
+        }
+        //Function To Display Popup
+        function show_popup() {
+        document.getElementById('isipopup').style.display = "block";
+        }
+        //Function to Hide Popup
+        function hide_popup(){
+        document.getElementById('isipopup').style.display = "none";
+        }
