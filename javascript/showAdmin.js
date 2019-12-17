0ca716226276ae257f490638ca70adb6c5996a49
nadhifrl <55243634+nadhifrl@users.noreply.github.com>
2019-12-17 07:20:56 +0700

    Create showAdmin.js

---
+++ javascript/showAdmin.js
@@ -0,0 +1,41 @@
+var masukanpass = document.getElementById('Password'),
+      chk  = document.getElementById('chk'),
+      label = document.getElementById('showhide');
+
+
+     chk.onclick = function () {
+
+      if(chk.checked) {
+
+           masukanpass.setAttribute('type', 'text');
+           label.textContent = 'Hide Password';
+       } else {
+
+           masukanpass.setAttribute('type', 'Password');
+           label.textContent = 'Show Password';
+       }
+ 
+     }
+
+     function Login() {
+        var nama = document.getElementById("Username").value;
+        var password = document.getElementById("Password").value;
+        if (nama != "pemri123" && password !="facebook123") {
+            alert('Username dan Password anda salah');
+        }else{
+            alert('Login Berhasil');
+            document.location="homeAdmin.html"   
+            
+        }
+    }
+
+    function validate() {
+        var password1 = document.getElementById('pw1').value;
+        var password2 = document.getElementById('pw2').value;
+        if (password1 != password2) {
+            alert("Password Tidak Sama!!");
+        } else {
+            alert("Selamat Anda Telah Mengganti Password Anda");
+            document.location="loginAdmin.html"
+        }
+    }
